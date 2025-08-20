ac72e88ce9d232ea516a7cee67b24ac507cd0d85
Arco Nightshade <arconightshade.raptorbytes@gmail.com>
2025-08-19 23:19:05 -0400

    Added the install script and some basic intro stuff to it

---
+++ install.sh
@@ -0,0 +1,11 @@
+# Raptorbytes, LLC LyonOS Installer v0.1.0
+
+echo "Most of the script doesnt work currently"
+echo "Welcome to the LyonOS installer, before we start, make sure you have these things:"
+echo "1. Internet"
+echo "2. A tty-only NixOS install"
+echo "3. Hope."
+echo ""
+
+echo "This installer does lots of things to your system, are you sure you want to continue? [Y/n]: "
+
