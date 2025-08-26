dbc308052d642fb6396b0c3dec6112bdc8bbae40
Arco Nightshade <arconightshade.raptorbytes@gmail.com>
2025-08-26 15:53:23 -0400

    Making a more fancy intro to the script

--- install.sh
+++ install.sh
@@ -1,6 +1,15 @@
 # Raptorbytes, LLC LyonOS Installer v0.1.0
 
-echo "Most of the script doesnt work currently"
+echo "    __                      ____  _____"
+echo "   / /   __  ______  ____  / __ \/ ___/"
+echo "  / /   / / / / __ \/ __ \/ / / /\__ \ "
+echo " / /___/ /_/ / /_/ / / / / /_/ /___/ / "
+echo "/_____/\__, /\____/_/ /_/\____//____/  "
+echo "      /____/                           "
+echo ""
+
+
+echo "Most (If not all) of the script doesnt work currently"
 echo "Welcome to the LyonOS installer, before we start, make sure you have these things:"
 echo "1. Internet"
 echo "2. A tty-only NixOS install"
